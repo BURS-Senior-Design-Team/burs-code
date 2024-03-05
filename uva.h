d9b201a08aa81248cf01a1d30634968b98f6b23b
alj21 <89426928+alj21@users.noreply.github.com>
2024-03-05 10:24:22 -0500

    Add files via upload

--- uva.h
+++ uva.h
@@ -1,13 +1,7 @@
 #ifndef uva_h
 #define uva_h
 
-
-#if (ARDUINO >= 100)
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-
+#include "Arduino.h"
 #include <Wire.h>
 
 #define LTR390_ADDRESS 0x53         ///< I2C address
